929525e3a84f6d7e30fd951c08ac46f0d8b116dc
Fatima <fatimaolasunkanmi01@gmail.com>
2020-08-20 02:03:26 +0100

    added components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>
+      <h1>NavBar leleyi</h1>
+      <button className='btn btn-primary'>This is Bootstrap button</button>
+    </div>
+  );
+}
+
+export default NavBar;
