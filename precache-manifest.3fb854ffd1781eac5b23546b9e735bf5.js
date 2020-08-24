8ed0bc08d4af52400f66515e5a8d0ecc0cc48626
Fatima <fatimaolasunkanmi01@gmail.com>
2020-08-24 17:55:41 +0100

    Updates

---
+++ precache-manifest.3fb854ffd1781eac5b23546b9e735bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e516c4ba7e14282d71d525f5aa93fef",
+    "url": "/fatima-facebook-app/index.html"
+  },
+  {
+    "revision": "cb5220389139b2193e38",
+    "url": "/fatima-facebook-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6a74d96e4b1994a00283",
+    "url": "/fatima-facebook-app/static/css/main.94176c8c.chunk.css"
+  },
+  {
+    "revision": "cb5220389139b2193e38",
+    "url": "/fatima-facebook-app/static/js/2.822cfd84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fatima-facebook-app/static/js/2.822cfd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a74d96e4b1994a00283",
+    "url": "/fatima-facebook-app/static/js/main.ce23d59f.chunk.js"
+  },
+  {
+    "revision": "3e6c0cf05a3eb8ba57b3",
+    "url": "/fatima-facebook-app/static/js/runtime-main.20c3f771.js"
+  }
+]);
